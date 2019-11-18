47333b74dd6b19987377914ec6c1797c1893ddbf
wasongapaul5 <wasongapaul5@gmail.com>
2019-11-18 12:06:40 +0300

    pass in parameters in the news object

---
+++ app/models/news.py
@@ -0,0 +1,12 @@
+class News:
+    '''
+    News class to define News objects
+    '''
+
+    def __init__(self,id,name,title,description,urlToimage,author)
+        self.id = id
+        self.name = name
+        self.title = title
+        self.description = description
+        self.urlToimage
+        self.author = author
