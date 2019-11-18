3c16291d3f060c5db9a14578b911f895556691af
wasongapaul5 <wasongapaul5@gmail.com>
2019-11-18 13:18:42 +0300

    get top headlines from our API

--- app/views.py
+++ app/views.py
@@ -26,3 +26,17 @@ def index():
 
     title = 'Home - Welcome to the best News Highlights Site Online'
     return render_template('index.html',title = title)
+
+
+from .request import get_news
+@app.route('/')
+def index():
+
+    '''
+    View root page that returns the index page and its data
+    '''
+
+    topHeadlines_news = get_news('topHeadlines')
+    print(top-headlines_news)
+    title = 'Home,Welcome to The best News Highlights Site Online'
+    return render_template('index.html', title = title,topHeadlines = top-headlines_news)
