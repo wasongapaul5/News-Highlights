802f5f26b2ab43eabf2bf377efd7957be4d87754
wasongapaul5 <wasongapaul5@gmail.com>
2019-11-18 09:54:45 +0300

    introduce a class config

---
+++ app/config.py
@@ -0,0 +1,4 @@
+class Config:
+    '''
+    General config parent class
+    '''
--- app/views.py
+++ app/views.py
@@ -9,4 +9,12 @@ def index():
     View root page function that returns the index page and it's data
     '''
     message = 'News Headlines'
-    return render_template('index.html',messge = message)
+    return render_template('index.html',message = message)
+
+@app.route('/news/<news_id>')
+def news(news_id):
+    '''
+    View news page function that returns the news details page and it's data
+    '''
+
+    return render_template('news.html',id = news_id)
