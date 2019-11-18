421fff1e71d941e9ca477d4280fe1910513eb343
wasongapaul5 <wasongapaul5@gmail.com>
2019-11-18 09:59:31 +0300

    enable debug mode

--- app/config.py
+++ app/config.py
@@ -8,3 +8,20 @@ class Config:
 class ProdConfig(Config):
     '''
     Production config child class
+
+    Args:
+        Config:The parent configuration class with General configuration serttings
+    '''
+
+    pass
+
+class DevConfig(Config):
+    '''
+    Development configi child class
+
+    Args:
+        config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+    
