9bf76158e102c08aebeefcb7f4743cf34cd3630f
wasongapaul5 <wasongapaul5@gmail.com>
2019-11-18 10:32:09 +0300

    add a base api key url

--- app/config.py
+++ app/config.py
@@ -3,25 +3,5 @@ class Config:
     General config parent class
     '''
 
-    pass
-
-class ProdConfig(Config):
-    '''
-    Production config child class
-
-    Args:
-        Config:The parent configuration class with General configuration serttings
-    '''
-
-    pass
-
-class DevConfig(Config):
-    '''
-    Development configi child class
-
-    Args:
-        config: The parent configuration class with General configuration settings
-    '''
-
-    DEBUG = True
+NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?category={}&apiKey={}''
     
