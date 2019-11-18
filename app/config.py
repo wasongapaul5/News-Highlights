5af64ead62d179f82b066a7534e1d3ec26ab20e9
wasongapaul5 <wasongapaul5@gmail.com>
2019-11-18 10:58:17 +0300

    import unitest

--- app/config.py
+++ app/config.py
@@ -3,5 +3,26 @@ class Config:
     General config parent class
     '''
 
-NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?category={}&apiKey={}''
+    pass
+
+class ProdConfig(Config):
+    '''
+    Production config child class
+
+    Args:
+        Config:The parent configuration class with General configuration serttings
+    '''
+
+    pass
+
+class DevConfig(Config):
+    '''
+    Development configi child class
+
+    Args:
+        config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
     
+NEWS_BASE_API_URL = 'https://newsapi.org/v2/sources?category={}&apiKey={}'
---
+++ app/news_test.py
@@ -0,0 +1,8 @@
+import unittest
+from models import news
+News = news.News
+
+class NewsTest(unittest.TestCase):
+    '''
+    Test class to test the behaviour of the News class
+    '''
