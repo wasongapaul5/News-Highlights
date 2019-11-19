584a4a764a9a1f6c0f7e9355f4c0416c6acc81f2
wasongapaul5 <wasongapaul5@gmail.com>
2019-11-19 16:00:30 +0300

    deploy to heroku

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,6 @@ from .config import DevConfig
 app = Flask(__name__,instance_relative_config= True)
 
 app.config.from_object(DevConfig)
-app.config.from_object('config.py')
+app.config.from_pyfile('config.py')
 
 from app import views
